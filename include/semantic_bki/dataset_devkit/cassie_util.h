3efa756c60f6ea3712d7245c6c3b13b6a52b961e
Lu Gan <ganlu@umich.edu>
2019-09-25 22:44:30 -0400

    added occupancy grid building

--- include/semantic_bki/dataset_devkit/cassie_util.h
+++ include/semantic_bki/dataset_devkit/cassie_util.h
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <math.h>
+#include <unordered_set>
 #include <chrono>
 
 #include <ros/ros.h>
@@ -12,7 +13,12 @@
 #include <pcl/common/transforms.h>
 #include <pcl/io/pcd_io.h>
 
+#include <nav_msgs/OccupancyGrid.h>
+
 using namespace std::chrono;
+
+// compute linear index for given map coords
+#define MAP_IDX(sx, i, j) ((sx) * (j) + (i))
 
 class CassieData {
   public:
@@ -31,6 +37,30 @@ class CassieData {
       , visualize_(visualize) {
         map_ = new semantic_bki::SemanticBKIOctoMap(resolution, block_depth, num_class, sf2, ell, prior, var_thresh, free_thresh, occupied_thresh);
         m_pub_ = new semantic_bki::MarkerArrayPub(nh_, map_topic_, resolution_);
+        
+        // for occupancy grid
+        occupancy_grid_ptr_ = std::make_shared<nav_msgs::OccupancyGrid>(nav_msgs::OccupancyGrid());
+        occupancy_grid_ptr_->info.resolution = 0.1;
+        occupancy_grid_ptr_->info.width = 2000;
+        occupancy_grid_ptr_->info.height = 2000;
+        occupancy_grid_ptr_->info.origin.position.x = -100.0;
+        occupancy_grid_ptr_->info.origin.position.y = -100.0;
+        occupancy_grid_ptr_->info.origin.position.z = 0.0;
+        occupancy_grid_ptr_->info.origin.orientation.x = 0.0;
+        occupancy_grid_ptr_->info.origin.orientation.y = 0.0;
+        occupancy_grid_ptr_->info.origin.orientation.z = 0.0;
+        occupancy_grid_ptr_->info.origin.orientation.w = 1.0;
+        occupancy_grid_ptr_->data.resize(occupancy_grid_ptr_->info.width * occupancy_grid_ptr_->info.height);
+        std::fill(occupancy_grid_ptr_->data.begin(), occupancy_grid_ptr_->data.end(), -1);
+        occupancy_grid_publisher_ = nh_.advertise<nav_msgs::OccupancyGrid>("occupancy_grid", 10);
+        
+        for (float i = -5; i <= 15; i += 0.1) {
+          xs.push_back(i);
+          ys.push_back(i);
+        //zs.push_back(i);
+        }
+        for (float i = -3; i < 0; i += 0.1)
+          zs.push_back(i);
       }
 
     // Data preprocess
@@ -88,6 +118,10 @@ class CassieData {
       
       if (visualize_)
         publish_map();
+
+      // for occupancy grid
+      std::unordered_set<int> target_labels{2,3};
+      build_occupancy_grid(cloud, origin,target_labels);
     }
 
     void publish_map() {
@@ -101,6 +135,52 @@ class CassieData {
       m_pub_->publish();
     }
 
+    void build_occupancy_grid(const semantic_bki::PCLPointCloud& cloud, const semantic_bki::point3f origin, std::unordered_set<int>& target_labels) {
+      
+      //int map_index = MAP_IDX(occupancy_grid_ptr_->info.width,
+                              //int((origin.x() - occupancy_grid_ptr_->info.origin.position.x) / occupancy_grid_ptr_->info.resolution ),
+                              //int((origin.y() - occupancy_grid_ptr_->info.origin.position.y) / occupancy_grid_ptr_->info.resolution));
+      //occupancy_grid_ptr_->data[map_index] = 100;
+      
+      
+      //for (int i = 0; i < cloud.points.size(); ++i) {
+      for (auto x : xs) {
+        x += origin.x();
+        for (auto y : ys) {
+        y += origin.y();
+          for(auto z : zs) {
+        //float x = cloud.points[i].x;
+        //float y = cloud.points[i].y;
+        //float z = cloud.points[i].z;
+        z += origin.z();
+
+        int map_index = MAP_IDX(occupancy_grid_ptr_->info.width,
+                                int((x - occupancy_grid_ptr_->info.origin.position.x) / occupancy_grid_ptr_->info.resolution ),
+                                int((y - occupancy_grid_ptr_->info.origin.position.y) / occupancy_grid_ptr_->info.resolution));
+        if (map_index >= occupancy_grid_ptr_->info.width * occupancy_grid_ptr_->info.height) {
+          continue;
+        }
+        if (occupancy_grid_ptr_->data[map_index] == 0) {
+          continue;
+        }
+
+        // search semantics
+        semantic_bki::SemanticOcTreeNode node = map_->search(x, y, z);
+        if (node.get_state() == semantic_bki::State::OCCUPIED) {
+          int label = node.get_semantics();
+          if (target_labels.find(label) != target_labels.end())
+            occupancy_grid_ptr_->data[map_index] = 0; // free
+          else
+            occupancy_grid_ptr_->data[map_index] = 100;
+        }
+      }
+        }
+      }
+
+        occupancy_grid_ptr_->header.frame_id = "/map";
+        occupancy_grid_publisher_.publish(*occupancy_grid_ptr_); 
+    }
+
   private:
     ros::NodeHandle nh_;
     double resolution_;
@@ -112,4 +192,11 @@ class CassieData {
     semantic_bki::SemanticBKIOctoMap* map_;
     semantic_bki::MarkerArrayPub* m_pub_;
     tf::TransformListener listener_;
-};
+    // for occupancy grid
+    std::shared_ptr<nav_msgs::OccupancyGrid> occupancy_grid_ptr_;
+    ros::Publisher occupancy_grid_publisher_;
+        std::vector<float> xs;
+        std::vector<float> ys;
+        std::vector<float> zs;
+
+    };
