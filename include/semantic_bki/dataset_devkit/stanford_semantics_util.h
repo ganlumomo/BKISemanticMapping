262c64c87e595db864d46558bf677995ee647a3a
Lu Gan <ganlu@umich.edu>
2019-11-18 13:51:08 -0500

    occupancy evaluation

---
+++ include/semantic_bki/dataset_devkit/stanford_semantics_util.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <fstream>
+#include <chrono>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+#include <pcl/common/transforms.h>
+#include <pcl/io/pcd_io.h>
+
+class StanfordSemanticsData {
+  public:
+    StanfordSemanticsData(ros::NodeHandle& nh,
+             double resolution, double block_depth,
+             double sf2, double ell,
+             int num_class, double free_thresh,
+             double occupied_thresh, float var_thresh, 
+	           double ds_resolution,
+             double free_resolution, double max_range,
+             std::string map_topic,
+             float prior)
+      : nh_(nh)
+      , resolution_(resolution)
+      , ds_resolution_(ds_resolution)
+      , free_resolution_(free_resolution)
+      , max_range_(max_range) {
+        map_ = new semantic_bki::SemanticBKIOctoMap(resolution, 1, num_class, sf2, ell, prior, var_thresh, free_thresh, occupied_thresh);
+        m_pub_ = new semantic_bki::MarkerArrayPub(nh_, map_topic, resolution);
+      	//init_trans_to_ground_ << 1, 0, 0, 0,
+          //                       0, 0, 1, 0,
+            //                     0,-1, 0, 1,
+              //                   0, 0, 0, 1;
+              //
+        init_trans_to_ground_ << 1, 0, 0, 0,
+                                 0, 1, 0, 0,
+                                 0, 0, 1, 0,
+                                 0, 0, 0, 1;
+      }
+
+    bool read_lidar_poses(const std::string lidar_pose_name) {
+      if (std::ifstream(lidar_pose_name)) {
+        std::ifstream fPoses;
+        fPoses.open(lidar_pose_name.c_str());
+        while (!fPoses.eof()) {
+          std::string s;
+          std::getline(fPoses, s);
+          if (!s.empty()) {
+            std::stringstream ss;
+            ss << s;
+            Eigen::Matrix4d t_matrix = Eigen::Matrix4d::Identity();
+            for (int i = 0; i < 3; ++i)
+              for (int j = 0; j < 4; ++j)
+                ss >> t_matrix(i, j);
+            lidar_poses_.push_back(t_matrix);
+          }
+        }
+        fPoses.close();
+        return true;
+        } else {
+         ROS_ERROR_STREAM("Cannot open evaluation list file " << lidar_pose_name);
+         return false;
+      }
+    } 
+
+    bool process_scans(std::string input_data_dir, std::string input_label_dir, int scan_num, bool query, bool visualize) {
+      semantic_bki::point3f origin;
+      
+      auto start = std::chrono::high_resolution_clock::now();
+      for (int scan_id  = 1; scan_id <= scan_num; ++scan_id) {
+        char scan_id_c[256];
+        sprintf(scan_id_c, "%06d", scan_id);
+        std::string scan_name = input_data_dir + std::string(scan_id_c) + ".pcd";
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::io::loadPCDFile<pcl::PointXYZ> (scan_name, *cloud);
+        pcl::PointCloud<pcl::PointXYZL>::Ptr labeled_cloud(new pcl::PointCloud<pcl::PointXYZL>);
+        for (int i = 0; i < cloud->points.size(); ++i) {
+          pcl::PointXYZL point;
+          point.x = cloud->points[i].x;
+          point.y = cloud->points[i].y;
+          point.z = cloud->points[i].z;
+          point.label = scan_id;
+          labeled_cloud->push_back(point);
+        }
+
+      	auto start = std::chrono::high_resolution_clock::now();
+	      map_->insert_pointcloud_csm(*labeled_cloud, origin, ds_resolution_, free_resolution_, max_range_);
+      	auto finish = std::chrono::high_resolution_clock::now();
+	      std::chrono::duration<double> elapsed = finish - start;
+      	std::cout << "Elapsed time for scan " << scan_name << " is: " << elapsed.count() << " s\n";
+        
+  
+        if (visualize)
+	        publish_map();
+      }
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = finish - start;
+      std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+      
+      if (query) {
+        query_scan(scan_num);
+      }
+
+      
+      return 1;
+    }
+
+    void publish_map() {
+      m_pub_->clear_map(resolution_);
+      for (auto it = map_->begin_leaf(); it != map_->end_leaf(); ++it) {
+        if (it.get_node().get_state() == semantic_bki::State::OCCUPIED) {
+          semantic_bki::point3f p = it.get_loc();
+          m_pub_->insert_point3d_semantics(p.x(), p.y(), p.z(), it.get_size(), it.get_node().get_semantics(), 2);
+        }
+      }
+      m_pub_->publish();
+    }
+
+    void set_up_evaluation(const std::string query_data_dir, const std::string evaluation_result_dir) {
+      query_data_dir_ = query_data_dir;
+      evaluation_result_dir_ = evaluation_result_dir;
+    }
+
+    void query_scan(int scan_num) {
+      for (int scan_id  = 1; scan_id <= scan_num; ++scan_id) {
+        char scan_id_c[256];
+        sprintf(scan_id_c, "%06d", scan_id);
+        std::string scan_name = query_data_dir_ + std::string(scan_id_c) + ".pcd";
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::io::loadPCDFile<pcl::PointXYZ> (scan_name, *cloud1);
+        
+        std::string result_name = evaluation_result_dir_ + std::string(scan_id_c) + ".txt";
+        
+        sprintf(scan_id_c, "%06d", scan_id + scan_num);
+        scan_name = query_data_dir_ + std::string(scan_id_c) + ".pcd";
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::io::loadPCDFile<pcl::PointXYZ> (scan_name, *cloud2);
+ 
+        std::ofstream result_file;
+        result_file.open(result_name);
+        for (int i = 0; i < cloud1->points.size(); ++i) {
+          int occupancy = 0;
+          for (float x = cloud1->points[i].x+0.01; x < cloud2->points[i].x; x += resolution_) {
+            for (float y = cloud1->points[i].y+0.01; y < cloud2->points[i].y; y += resolution_) {
+              for (float z = cloud1->points[i].z+0.01; z < cloud2->points[i].z; z += resolution_) {
+                semantic_bki::SemanticOcTreeNode node = map_->search(x, y, z);
+                if (node.get_state() == semantic_bki::State::OCCUPIED)
+                  occupancy = 1;
+              }
+            }
+          }
+          result_file << occupancy << "\n";
+        }
+        result_file.close();
+       }
+    }
+
+  
+  private:
+    ros::NodeHandle nh_;
+    double resolution_;
+    double ds_resolution_;
+    double free_resolution_;
+    double max_range_;
+    semantic_bki::SemanticBKIOctoMap* map_;
+    semantic_bki::MarkerArrayPub* m_pub_;
+    //ros::Publisher color_octomap_publisher_;
+    tf::TransformListener listener_;
+    std::ofstream pose_file_;
+    std::vector<Eigen::Matrix4d> lidar_poses_;
+    std::string query_data_dir_;
+    std::string evaluation_result_dir_;
+    Eigen::Matrix4d init_trans_to_ground_;
+
+    int check_element_in_vector(const long long element, const std::vector<long long>& vec_check) {
+      for (int i = 0; i < vec_check.size(); ++i)
+        if (element == vec_check[i])
+          return i;
+      return -1;
+    }
+
+};
---
+++ src/stanford_semantics_node.cpp
@@ -0,0 +1,97 @@
+#include <string>
+#include <iostream>
+#include <ros/ros.h>
+
+#include "bkioctomap.h"
+#include "markerarray_pub.h"
+#include "stanford_semantics_util.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "2d3dsemantics_node");
+    ros::NodeHandle nh("~");
+
+    std::string map_topic("/occupied_cells_vis_array");
+    int block_depth = 4;
+    double sf2 = 1.0;
+    double ell = 1.0;
+    float prior = 1.0f;
+    float var_thresh = 1.0f;
+    double free_thresh = 0.3;
+    double occupied_thresh = 0.7;
+    double resolution = 0.1;
+    int num_class = 2;
+    double free_resolution = 0.5;
+    double ds_resolution = 0.1;
+    int scan_num = 0;
+    double max_range = -1;
+    
+    // SemanticKITTI
+    std::string dir;
+    std::string input_data_prefix;
+    std::string input_label_prefix;
+    std::string lidar_pose_file;
+    std::string query_data_prefix;
+    std::string evaluation_result_prefix;
+    bool query = false;
+    bool visualize = false;
+
+    nh.param<int>("block_depth", block_depth, block_depth);
+    nh.param<double>("sf2", sf2, sf2);
+    nh.param<double>("ell", ell, ell);
+    nh.param<float>("prior", prior, prior);
+    nh.param<float>("var_thresh", var_thresh, var_thresh);
+    nh.param<double>("free_thresh", free_thresh, free_thresh);
+    nh.param<double>("occupied_thresh", occupied_thresh, occupied_thresh);
+    nh.param<double>("resolution", resolution, resolution);
+    nh.param<int>("num_class", num_class, num_class);
+    nh.param<double>("free_resolution", free_resolution, free_resolution);
+    nh.param<double>("ds_resolution", ds_resolution, ds_resolution);
+    nh.param<int>("scan_num", scan_num, scan_num);
+    nh.param<double>("max_range", max_range, max_range);
+
+    // SemanticKITTI
+    nh.param<std::string>("dir", dir, dir);
+    nh.param<std::string>("input_data_prefix", input_data_prefix, input_data_prefix);
+    nh.param<std::string>("input_label_prefix", input_label_prefix, input_label_prefix);
+    nh.param<std::string>("lidar_pose_file", lidar_pose_file, lidar_pose_file);
+    nh.param<std::string>("query_data_prefix", query_data_prefix, query_data_prefix);
+    nh.param<std::string>("evaluation_result_prefix", evaluation_result_prefix, evaluation_result_prefix);
+    nh.param<bool>("query", query, query);
+    nh.param<bool>("visualize", visualize, visualize);
+
+    ROS_INFO_STREAM("Parameters:" << std::endl <<
+      "block_depth: " << block_depth << std::endl <<
+      "sf2: " << sf2 << std::endl <<
+      "ell: " << ell << std::endl <<
+      "prior:" << prior << std::endl <<
+      "var_thresh: " << var_thresh << std::endl <<
+      "free_thresh: " << free_thresh << std::endl <<
+      "occupied_thresh: " << occupied_thresh << std::endl <<
+      "resolution: " << resolution << std::endl <<
+      "num_class: " << num_class << std::endl << 
+      "free_resolution: " << free_resolution << std::endl <<
+      "ds_resolution: " << ds_resolution << std::endl <<
+      "scan_num: " << scan_num << std::endl <<
+      "max_range: " << max_range << std::endl <<
+
+      "SemanticKITTI:" << std::endl <<
+      "dir: " << dir << std::endl <<
+      "input_data_prefix: " << input_data_prefix << std::endl <<
+      "input_label_prefix: " << input_label_prefix << std::endl <<
+      "lidar_pose_file: " << lidar_pose_file << std::endl <<
+      "query_data_prefix: " << query_data_prefix << std::endl << 
+      "evaluation_result_prefix: " << evaluation_result_prefix << std::endl <<
+      "query: " << query << std::endl <<
+      "visualize:" << visualize
+      );
+
+    
+    ///////// Build Map /////////////////////
+    StanfordSemanticsData stanford_semantics_data(nh, resolution, block_depth, sf2, ell, num_class, free_thresh, occupied_thresh, var_thresh, ds_resolution, free_resolution, max_range, map_topic, prior);
+    stanford_semantics_data.read_lidar_poses(dir + '/' + lidar_pose_file);
+    stanford_semantics_data.set_up_evaluation(dir + '/' + query_data_prefix, dir + '/' + evaluation_result_prefix);
+    stanford_semantics_data.process_scans(dir + '/' + input_data_prefix, dir + '/' + input_label_prefix, scan_num, query, visualize);
+
+    ros::spin();
+    return 0;
+}
